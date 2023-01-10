a5f115c7c11dd792dafacaf5bae48521616b92f0
matin dezhbani <dezhbani.matin@gmail.com>
2023-01-10 20:49:57 +0330

    socket io

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require("http");
-const server = http.createServer();
+const express = require("express");
+const app = express();
+app.use(express.static("./"))
+const server = http.createServer(app);
 const socketIO = require("socket.io");
 const io = socketIO(server, {
     cors:{
